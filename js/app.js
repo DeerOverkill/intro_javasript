b7bc2119964886d9d52ce47d997f9202b91e1702
DeerOverkill <henriquecarvalhodasilvapedro3@gmail.com>
2026-04-29 09:20:16 -0300

    java script externo

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("botao-ok");
+        console.log(botao.textContent);
+
+
+        let idade = 32;
+        console.log(idade);
+        idade = 24;
+        console.log(idade);
+        
